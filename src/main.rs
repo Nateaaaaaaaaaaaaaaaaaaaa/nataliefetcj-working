38aafde9a95e78ab36cf906ef6503c68ddfe6b75
natalie <natedranoff@gmail.com>
2024-08-23 06:51:38 -0400

    anime girl+swap

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,16 @@ fn main() {
     };
     let mut sys = System::new_all();
     sys.refresh_all();
-    println!("OS: {:?}", System::name());
-    println!("Kernel version: {:?}", System::kernel_version());
-    println!("Total memory: {} KB", sys.total_memory());
-    println!("Used memory: {} KB", sys.used_memory());
-    println!("Free memory: {} KB", sys.free_memory());
+    println!(" ⣿⣿⣿⠿⠿⣿⣿⡿⢋⣶⣶⣬⣙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿  OS: {:?}", System::name());
+    println!(" ⣿⡿⢡⣿⣷⣶⣦⣥⣿⣿⣿⣿⣿⣷⣮⡛⢿⣿⣿⣿⣿⣿⣿⣿  Kernel version: {:?}", System::kernel_version());
+    println!(" ⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢮⡙⣿⣿⣯⢐⡎⣿  Total memory: {} KB", sys.total_memory());
+    println!(" ⣿⢹⣿⣿⣿⣿⣿⣿⡿⣡⡬⢿⣿⣿⣿⣶⣶⣼⣦⠥⣖⣩⣾⣿  Used memory: {} KB", sys.used_memory());
+    println!(" ⣿⢸⣿⣿⣿⡿⣿⣿⣿⣿⠇⣌⢛⣻⣿⣿⣟⣛⣿⣧⠹⣿⣿⣿  Free memory: {} KB", sys.free_memory());
+    println!(" ⠏⣼⣿⣿⢏⣾⣿⣟⣩⣶⣶⣿⣿⣿⣿⣿⡟⡿⢸⡿⣡⣿⣿⣿  Total swap: {} KB", sys.total_swap());
+    println!(" ⣼⣿⣿⠇⣼⣿⣿⢸⠋⠁⠉⢽⣿⣿⣿⣟⣠⣤⣆⢃⢻⣿⣿⣿");
+    println!(" ⣿⣿⣿⣼⣿⣿⣿⡞⣿⣿⣷⣾⣿⣿⣿⣿⡿⠟⠛⠸⢦⣙⡋⣿");
+    println!(" ⣿⣿⣿⠹⣿⣿⡿⠗⣈⣭⣭⣭⣉⠻⡟⣩⣶⣾⣿⣿⣶⡙⣱⣿");
+    println!(" ⣿⣿⣿⣷⣌⡛⠠⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⢸⣿");
+    println!(" ⣿⣿⣿⣿⢏⣴⣧⣴⡘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣱⣶⣴⡜⢸⣿");
+    println!(" ⣿⣿⣿⢃⣾⣿⣿⣿⡷⠉⢿⣿⣿⣿⣿⣿⣿⢰⣾⣿⣿⣧⢸⣿ ");
 }
