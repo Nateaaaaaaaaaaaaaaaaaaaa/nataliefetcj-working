62b3c124441f50cc1fe34e3fdee7f29b6cf714d9
natalie <natedranoff@gmail.com>
2024-08-23 01:19:11 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::mem::replace;
+
+
+fn main() {
+    use sysinfo::{
+        Components, Disks, Networks, System,
+    };
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    println!("OS: {:?}", System::name());
+    println!("Kernel version: {:?}", System::kernel_version());
+    println!("Total memory: {} KB", sys.total_memory());
+    println!("Used memory: {} KB", sys.used_memory());
+    println!("Free memory: {} KB", sys.free_memory());
+}
